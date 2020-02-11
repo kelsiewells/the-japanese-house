4572714cf4cedb213d6404bffd68dded5619ef60
kelsiewells <59620231+kelsiewells@users.noreply.github.com>
2020-02-10 20:45:43 -0800

    get on github

--- js/script.js
+++ js/script.js
@@ -2,19 +2,19 @@
 $(function() {
 // Your Code from here on down. Don't delete that line above!
   
+<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+<script> $('#mybutton').hide(); // this or use css to hide the div
+$('#mybutton').delay(2000).fadeIn('slow');</script>
+
+<script>
+  $(document).ready(function() {    $('#mybutton').hide().fadeIn('slow');
+  });
+</script>
+
+	<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-// End of Your Code . Don't delete that line below!!
-});
+// End of Your Code . Don't delete that line below!!});
